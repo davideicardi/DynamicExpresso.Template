ebf84a852c2f120a9b7fb1374c826708ed30fb7c
Davide Icardi <davide.icardi@gmail.com>
2017-11-03 10:48:21 +0100

    Added expression catalog

--- DynamicExpresso.Template.Test/TemplateEngineTest.cs
+++ DynamicExpresso.Template.Test/TemplateEngineTest.cs
@@ -1,25 +1,112 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace DynamicExpresso.Template.Test
 {
 	[TestFixture]
 	public class TemplateEngineTest
 	{
-		[TestCase(null, null)]
-		[TestCase("", "")]
-		[TestCase("<p>Ciao</p>", "<p>Ciao</p>")]
-		[TestCase("<p>@Model.Method1()</p>", "<p>output 1</p>")]
-		[TestCase("<p>@Model.Method1() (@Model.Method1())</p>", "<p>output 1 (output 1)</p>")]
-		[TestCase("<p class=\"@Model.Method1()\">bla bla</p>", "<p class=\"output 1\">bla bla</p>")]
-		[TestCase("<p class=\"@Model.Hello(@Model.Method1())\">bla bla</p>", "<p class=\"Hello output 1!\">bla bla</p>")]
-		[TestCase("<p>@Model.Method1() (@Model.Method1())</p>", "<p>output 1 (output 1)</p>")]
-		[TestCase("<p>@Model.Property1</p>", "<p>my property 1</p>")]
-		[TestCase("<p>@Model.Hello(\"world\")</p>", "<p>Hello world!</p>")]
-		[TestCase("<p>@Model.Hello(@Model.Method1())</p>", "<p>Hello output 1!</p>")]
-		[TestCase("<p>@Model.Concat(\"Hello\", \" world!\")</p>", "<p>Hello world!</p>")]
+		[Test]
+		public void Demo()
+		{
+			var engine = new TemplateEngine<DateTime>();
+
+			var model = DateTime.Now;
+			const string template = "<div>@Model.Millisecond</div>";
+
+			var result = engine.Render(template, model);
+
+			var expected = $"<div>{model.Millisecond}</div>";
+			Assert.AreEqual(expected, result);
+		}
+
+		[Test]
+		public void Same_expression_with_different_model()
+		{
+			var engine = new TemplateEngine<DateTime>();
+
+			const string template = "<div>@Model.ToOADate()</div>";
+
+			var model = DateTime.Now;
+			var result = engine.Render(template, model);
+			var expected = $"<div>{model.ToOADate()}</div>";
+			Assert.AreEqual(expected, result);
+
+			var model2 = DateTime.Now;
+			var result2 = engine.Render(template, model2);
+			var expected2 = $"<div>{model2.ToOADate()}</div>";
+			Assert.AreEqual(expected2, result2);
+		}
+
+		[TestCase(
+			null,
+			null)]
+		[TestCase(
+			"",
+			"")]
+		[TestCase(
+			"<p>Ciao</p>",
+			"<p>Ciao</p>")]
+		[TestCase(
+			"<p>@Model.Method1()</p>",
+			"<p>output 1</p>")]
+		[TestCase(
+			"<p>@Model.Method1() (@Model.Method1())</p>",
+			"<p>output 1 (output 1)</p>")]
+		[TestCase(
+			"<p class=\"@Model.Method1()\">bla bla</p>",
+			"<p class=\"output 1\">bla bla</p>")]
+		[TestCase(
+			"<p class=\"@Model.Hello(@Model.Method1())\">bla bla</p>",
+			"<p class=\"Hello output 1!\">bla bla</p>")]
+		[TestCase(
+			"<p>@Model.Method1() (@Model.Method1())</p>",
+			"<p>output 1 (output 1)</p>")]
+		[TestCase(
+			"<p>@Model.Property1</p>",
+			"<p>my property 1</p>")]
+		[TestCase(
+			"<p>@Model.Hello(\"world\")</p>",
+			"<p>Hello world!</p>")]
+		[TestCase(
+			"<p>@Model.Hello(@Model.Method1())</p>",
+			"<p>Hello output 1!</p>")]
+		[TestCase(
+			"<p>@Model.Concat(\"Hello\", \" world!\")</p>",
+			"<p>Hello world!</p>")]
 		public void Render(string template, string expected)
 		{
 			var engine = new TemplateEngine<MyModel>();
+
+			Assert.AreEqual(expected, engine.Render(template, new MyModel()));
+		}
+
+		[TestCase(
+	"<p>@Model.MethodInt()</p>",
+	"<p>55</p>")]
+		public void Render_with_conversion(string template, string expected)
+		{
+			var engine = new TemplateEngine<MyModel>();
+
+			Assert.AreEqual(expected, engine.Render(template, new MyModel()));
+		}
+
+		[Test]
+		public void Render_multi_line()
+		{
+			var engine = new TemplateEngine<MyModel>();
+
+			const string template = @"<div>
+<span class=""@Model.Concat(""Hello"", "" world!"")"">
+	@Model.Hello(@Model.Method1())
+</span>
+</div>";
+
+			const string expected = @"<div>
+<span class=""Hello world!"">
+	Hello output 1!
+</span>
+</div>";
 
 			Assert.AreEqual(expected, engine.Render(template, new MyModel()));
 		}
@@ -32,6 +119,11 @@ namespace DynamicExpresso.Template.Test
 		public string Method1()
 		{
 			return "output 1";
+		}
+
+		public int MethodInt()
+		{
+			return 55;
 		}
 
 		public string Hello(string target)
--- DynamicExpresso.Template/TemplateEngine.cs
+++ DynamicExpresso.Template/TemplateEngine.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Text.RegularExpressions;
 
 namespace DynamicExpresso.Template
 {
 	public class TemplateEngine<T>
 	{
+		private readonly ConcurrentDictionary<string, Func<T, object>> 
+			_expressionCatalog = new ConcurrentDictionary<string, Func<T, object>>();
+
 		private readonly Interpreter _interpreter = new Interpreter();
 
 		public string Render(string template, T model)
@@ -19,13 +23,30 @@ namespace DynamicExpresso.Template
 
 		private string Replacer(Match match, T model)
 		{
-			var expression = match.Value.Replace("@Model.", "Model.");
+			var expression = NormalizeExpression(match);
 
-			var expressionDelegate = _interpreter.ParseAsDelegate<Func<T, string>>(
+			var expDelegate = GetOrCreateDelegate(expression);
+
+			return expDelegate(model)?.ToString();
+		}
+
+		private Func<T, object> GetOrCreateDelegate(string expression)
+		{
+			return _expressionCatalog.GetOrAdd(expression, CreateDelegate);
+		}
+
+		private static string NormalizeExpression(Match match)
+		{
+			var expression = match.Value.Replace("@Model.", "Model.");
+			return expression;
+		}
+
+		private Func<T, object> CreateDelegate(string expression)
+		{
+			var expressionDelegate = _interpreter.ParseAsDelegate<Func<T, object>>(
 				expression,
 				"Model");
-
-			return expressionDelegate(model);
+			return expressionDelegate;
 		}
 	}
 
