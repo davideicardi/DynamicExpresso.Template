70119348ce640a01552012277a3291c45df69174
Davide Icardi <davide.icardi@gmail.com>
2017-11-03 15:32:46 +0100

    Added tests

--- DynamicExpresso.Template.Performance/Program.cs
+++ DynamicExpresso.Template.Performance/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace DynamicExpresso.Template.Performance
@@ -7,6 +8,47 @@ namespace DynamicExpresso.Template.Performance
 	public class Program
 	{
 		public static void Main()
+		{
+			Console.WriteLine("DynamicExpresso.Template performance tests:");
+			Console.WriteLine("A - run automatic tests");
+			Console.WriteLine("F - render file");
+
+			switch (Console.ReadLine())
+			{
+				case "A":
+					AutoPerformanceTests();
+					break;
+				case "F":
+					RenderFile();
+					break;
+			}
+
+			Console.WriteLine("Enter to exit");
+			Console.ReadLine();
+		}
+
+		private static void RenderFile()
+		{
+			var engine = new TemplateEngine<MyModel>();
+
+			Console.WriteLine("Enter file name:");
+
+			var file = Console.ReadLine();
+			var template = File.ReadAllText(file);
+
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			var result = engine.Render(template, new MyModel());
+
+			stopWatch.Stop();
+
+			File.WriteAllText(file + ".out", result);
+
+			Console.WriteLine("Elapsed ms: " + stopWatch.Elapsed.TotalMilliseconds);
+		}
+
+		private static void AutoPerformanceTests()
 		{
 			MultipleTemplates(100000);
 
@@ -90,6 +132,61 @@ namespace DynamicExpresso.Template.Performance
 		public string Hello(string target)
 		{
 			return $"Hello {target}!";
+		}
+
+		public string Translate(string value)
+		{
+			return "Translate:" + value;
+		}
+
+		public string GetAthletePopoutUrl(string value)
+		{
+			return "GetAthletePopoutUrl:" + value;
+		}
+
+		public string GetAthleteShortName(string value)
+		{
+			return "GetAthleteShortName:" + value;
+		}
+
+		public string GetAthleteFullName(string value)
+		{
+			return "GetAthleteFullName:" + value;
+		}
+
+		public string GetAthletePageUrl(string value)
+		{
+			return "GetAthletePageUrl:" + value;
+		}
+
+		public string GetAthleteSurname(string value)
+		{
+			return "GetAthleteSurname:" + value;
+		}
+
+		public string GetAthleteCountryName(string value)
+		{
+			return "GetAthleteCountryName:" + value;
+		}
+
+		public string GetTransparentFlag(string value)
+		{
+			return "GetTransparentFlag:" + value;
+		}
+
+		public string GetAthleteNoc(string value)
+		{
+			return "GetAthleteNoc:" + value;
+		}
+
+		public string ParamTranslate(string value, string param)
+		{
+			return "ParamTranslate:" + value + param;
+		}
+
+		public string GetFlag(string value, string param)
+		{
+			return "GetFlag:" + value + param;
 		}
 	}
 }
